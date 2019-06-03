e90303bc5fa09dd821d3ad38a5c38298e93ad7a2
Arvin Gatela <acgatela78@hotmail.com>
2019-06-03 16:19:46 -0400

    learning about json to python

--- 02-jsonTOpython.py
+++ 02-jsonTOpython.py
@@ -9,12 +9,16 @@ def main():
     with open("jonsnow.json", "r") as gotdata:
         jonsnow = gotdata.read() # create a STRING of all the json
         GOTpy = json.loads(jonsnow) # convert STRING to pythonic LISTs and DISCTs
-    #print(GOTpy) 
-    print(GOTpy["url"])
-    #print(GOTpy["titles"][0])
-    print(GOTpy["aliases"])
-    for gotalias in GOTpy["aliases"]:
-        print(gotalias)
+    #print(GOTpy) # print GOTpy data 
+    print(GOTpy["url"]) # print GOTpy url
+    #print(GOTpy["titles"][0]) # print GOTpy titles
+    print(GOTpy["aliases"])  # print GOTpy aliases
+    
+    # create 
+    with open("aliases.txt", "w") as jsaliases:
+        for gotalias in GOTpy["aliases"]:
+           print(gotalias, file=jsaliases)
+    
     #print(jonsnow["url"]
     # parse the jonsnow.json file for...
     # display character name
